a0731ad018afe43ba1aaa2284a87c608ca7eafae
Zee <chimasamuel6@gmail.com>
2024-11-21 16:21:12 +0100

    testing

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,11 +2,17 @@
 """Create a USER Model using sqlalchemy
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+    )
 Base = declarative_base()
 
 class User(Base):
+    """
+    Definition of a class User
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True, nullable=False)
